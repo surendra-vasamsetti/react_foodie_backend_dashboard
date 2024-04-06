46131dbd02918fb9cb6b724aebb26c7c69d54ba3
Surendra Vasamsetti <leelasurendravasamsetti@gmail.com>
2024-04-07 01:38:19 +0530

    change api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
- export const API_URL = "http://localhost:4000"
+//  export const API_URL = "http://localhost:4000"
 
-// export const API_URL ="https://backend-nodejs-foodie.onrender.com"
+export const API_URL ="https://backend-nodejs-foodie.onrender.com"
